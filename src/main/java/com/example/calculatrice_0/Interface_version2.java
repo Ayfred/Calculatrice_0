9fead16b6ebd60d3e9d48608730ca8ed65d540cf
Ayfred <maxime.mu@mines-ales.org>
2022-10-21 16:20:36 +0200

    v1.08

--- src/main/java/com/example/calculatrice_0/Interface_version2.java
+++ src/main/java/com/example/calculatrice_0/Interface_version2.java
@@ -4,21 +4,16 @@ package com.example.calculatrice_0;
 
 import javafx.application.Application;
 import javafx.geometry.Insets;
-import javafx.geometry.Pos;
 import javafx.scene.Scene;
-import javafx.scene.control.*;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
+import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.*;
 import javafx.scene.paint.Color;
 import javafx.scene.shape.Circle;
 import javafx.scene.text.Font;
 import javafx.scene.text.FontWeight;
 import javafx.stage.Stage;
-
-import javax.swing.text.Position;
-import java.awt.*;
-import java.util.Stack;
 
 
 public class Interface_version2 extends Application {
@@ -27,15 +22,9 @@ public class Interface_version2 extends Application {
     @Override
     public void start(Stage stage) {
 
-        //instanciation
-        String version = System.getProperty("java.version");
-
         //creation de nouveaux objets
-        Label l = new Label ("Hello, JavaFX 13, running on Java version " + version);
-        Label label1 = new Label ( "Bonjour ! ");
         VBox box = new VBox();
         StackPane stackPane = new StackPane();
-        Pane pane = new Pane();
 
         createButtons(stackPane);
 
@@ -44,13 +33,9 @@ public class Interface_version2 extends Application {
 
 
         //Gridpane centered
-        //gridpane.setAlignment(Pos.BOTTOM_CENTER);
-        stackPane.setTranslateX(-140); stackPane.setTranslateY(-30);
+        stackPane.setTranslateX(-140); stackPane.setTranslateY(10);
 
         //Vbox setting up
-        //box.getChildren().add(pane);
-        //box.getChildren().add(button0);
-        //box.getChildren().add(l);
         box.getChildren().add(stackPane);
 
         //création
@@ -63,15 +48,19 @@ public class Interface_version2 extends Application {
     }
 
 
+
+
     public void createButtons(StackPane sp){
-        String[] nomBoutons = {"0", "1", "2", "3", "4", "5", "6", "7", "8", "9", "clear",
+        String[] nomBoutons = {"0", "1", "2", "3", "4", "5", "6", "7", "8", "9", "C",
                                 "push", "+", "-", "x", "/", "-", ","};
         int[][] pos = {{2,399},{2,323},{92,323},{180,323},{2,248},{92,248},{180,248},{2,172},{92,172},{180,172},
                             {2,98},{268,399},{268,322},{268,248},{268,173},{268,98},{92,399},{180,399}};
         double x = 70; double y = 70;
 
         Label resultat = new Label("0.0");
+        resultat.setTranslateY(20); resultat.setTranslateX(10);
         resultat.setTextFill(Color.WHITE);
+        resultat.setFont(Font.font("Courier New", FontWeight.BOLD, 36));
 
         for(int i = 0; i < nomBoutons.length; i++){
             Button button = new Button(nomBoutons[i]);
@@ -95,6 +84,7 @@ public class Interface_version2 extends Application {
                 if(i==10){
                     button. setStyle("-fx-background-color: #808080");
                     button.setTextFill(Color.BLACK);
+                    button.setFont(Font.font("Courier New", FontWeight.BOLD, 36));
                 }
                 else{
                     if(i==17){
@@ -111,6 +101,25 @@ public class Interface_version2 extends Application {
             }
         }
         sp.getChildren().addAll(resultat);
+    }
+
+    public void onNumberClicked(MouseEvent event) {
+        String valeur = switch (((Pane) event.getSource()).getId()) {
+            case "0" -> "0";
+            case "1" -> "1";
+            case "2" -> "2";
+            case "3" -> "3";
+            case "4" -> "4";
+            case "5" -> "5";
+            case "6" -> "6";
+            case "7" -> "7";
+            case "8" -> "8";
+            case "9" -> "9";
+            default -> "";
+        };
+        if(result == "0.0" || result == "0"){ result = valeur;}
+        else{result += valeur;}
+        resultat.setText(result);
     }
 
     public static void main(String[] args) {
